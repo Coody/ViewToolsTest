d24401c1b972d1ca7da38e5c66d8f33dbcbb0abc
Coody <coody0829@gmail.com>
2016-04-13 14:58:38 +0800

    [ refactor ] [ ViewTools ] 減少工具類別的耦合性
    1. 將 define 拆出去由 constant.h 來設定個別所需要的環境設定，避免與工具類別過於耦合。

---
+++ ViewToolsProject/ViewToolsClass/ViewToolsConstant.h
@@ -0,0 +1,42 @@
+//
+//  ViewToolsConstant.h
+//  Prime
+//
+//  Created by Coody on 2016/1/30.
+//  Copyright © 2016年 Coody. All rights reserved.
+//
+
+#ifndef ViewToolsConstant_h
+#define ViewToolsConstant_h
+
+////////// 預設的畫面顯示設定（如果沒有特別要求的話，會吃的預設值）//////////
+///////////////////////////////////////////////////////////////////
+// 設定元件標準高度（可自行設定）
+#define D_ViewTools_ViewHeight (40.0f)
+// 設定元件與左邊畫面的距離
+#define D_ViewTools_Label_Left_Margin (12)
+#define D_ViewTools_Label_Middle_Margin (12)
+#define D_ViewTools_Label_Right_Margin (12)
+// 設定文字大小
+#define D_ViewTools_Text_Font [UIFont boldSystemFontOfSize:16.0f]
+// 設定文字顏色
+#define D_ViewTools_Text_Color [UIColor whiteColor]
+// 設定 TextField 內文字的顏色
+#define D_ViewTools_TextField_Inner_Color [UIColor grayColor]
+///////////////////////////////////////////////////////////////////
+// 如果不需要設定請直接使用 nil 即可
+// 設定 Image 的名稱（不帶 .png ）
+#define D_ViewTools_Arrow_Image (@"arrow")
+// 設定紅色按鈕的 Image
+#define D_ViewTools_Button_Red_Normal_Image (@"red_btn")
+#define D_ViewTools_Button_Red_HightLight_Image (nil)
+// 設定一般類型按鈕的 Image 1
+#define D_ViewTools_Button_Normal_Image (@"gray_btn")
+#define D_ViewTools_Button_HightLight_Image (nil)
+#define D_ViewTools_Button_Disable_Image (nil)
+// 設定輸入框類型的 Image
+#define D_ViewTools_TextField_Image (@"box_textField")
+#define D_ViewTools_TextField_CancelButton_Image (nil)
+///////////////////////////////////////////////////////////////////
+
+#endif /* ViewToolsConstant_h */
